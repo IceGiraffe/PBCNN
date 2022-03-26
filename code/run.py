0b0c2e5efdab3592e4b1f9cd781a5366c23ffd53
Wang Feiyu <137204354@qq.com>
2022-03-26 16:53:25 +0800

    format

--- code/run.py
+++ code/run.py
@@ -13,9 +13,9 @@ from tensorflow.keras import Input
 from tensorflow.keras import layers, Model
 from tensorflow.keras.layers import Layer
 
-MAX_PKT_BYTES = 50 * 50
-MAX_PKT_NUM = 100
-AUTOTUNE = tf.data.experimental.AUTOTUNE
+MAX_PKT_BYTES = 50 * 50  # 每个包最大的字节数
+MAX_PKT_NUM = 100       # 最大的包数
+AUTOTUNE = tf.data.experimental.AUTOTUNE    # 数据读取加速
 
 
 def plot_heatmap(report, y_labels=None):
@@ -49,7 +49,10 @@ class TF(object):
     def __init__(self, pkt_bytes, pkt_num, model,
                  train_path, valid_path, test_path,
                  batch_size=128, num_class=15):
+        #  the name of model, lower it and strip it
         model = model.lower().strip()
+        print(f'model name: {model}')
+
         assert pkt_bytes <= MAX_PKT_BYTES, f'Check pkt bytes less than max pkt bytes {MAX_PKT_BYTES}'
         assert pkt_num <= MAX_PKT_NUM, f'Check pkt num less than max pkt num {MAX_PKT_NUM}'
         assert model in ('pbcnn', 'en_pbcnn'), f'Check model type'
@@ -73,7 +76,10 @@ class TF(object):
         if not os.path.exists(self._prefix):
             os.makedirs(self._prefix)
 
+    # __new__方法是python中创建对象的方法，__init__是初始化方法
+    # 创建一个新的对象，先__new__，在__init__
     def __new__(cls, *args, **kwargs):
+        print(f'new method with no gpu-associated code')
         # os.environ['CUDA_VISIBLE_DEVICES'] = '0'
         logging.set_verbosity(logging.INFO)
         tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.WARN)
@@ -83,14 +89,14 @@ class TF(object):
         # tf.config.threading.set_inter_op_parallelism_threads(0)
         # tf.config.threading.set_intra_op_parallelism_threads(0)
 
-        gpus = tf.config.list_physical_devices('GPU')
-        if gpus:
-            try:
-                tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
-                tf.config.experimental.set_memory_growth(gpus[0], True)
-            except RuntimeError as e:
-                # Visible devices must be set before GPUs have been initialized
-                print(e)
+        # gpus = tf.config.list_physical_devices('GPU')
+        # if gpus:
+        #     try:
+        #         tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+        #         tf.config.experimental.set_memory_growth(gpus[0], True)
+        #     except RuntimeError as e:
+        #         # Visible devices must be set before GPUs have been initialized
+        #         print(e)
         return super().__new__(cls)
 
     def _parse_sparse_example(self, example_proto):
@@ -106,16 +112,19 @@ class TF(object):
         batch_sample = tf.io.parse_example(example_proto, features)
         sparse_features = batch_sample['sparse']
         labels = batch_sample['label']
-        sparse_features = tf.sparse.slice(sparse_features, start=[0, 0], size=[self._pkt_num, self._pkt_bytes])
+        sparse_features = tf.sparse.slice(sparse_features, start=[0, 0], size=[
+                                          self._pkt_num, self._pkt_bytes])
         dense_features = tf.sparse.to_dense(sparse_features)
         dense_features = tf.cast(dense_features, tf.float32) / 255.
         return dense_features, labels
 
     def _generate_ds(self, path_dir, use_cache=False):
         assert os.path.isdir(path_dir)
-        ds = tf.data.Dataset.list_files(os.path.join(path_dir, '*.tfrecord'), shuffle=True)
+        ds = tf.data.Dataset.list_files(
+            os.path.join(path_dir, '*.tfrecord'), shuffle=True)
         ds = ds.interleave(
-            lambda x: tf.data.TFRecordDataset(x).map(self._parse_sparse_example),
+            lambda x: tf.data.TFRecordDataset(
+                x).map(self._parse_sparse_example),
             cycle_length=AUTOTUNE,
             block_length=8,
             num_parallel_calls=AUTOTUNE
@@ -134,15 +143,18 @@ class TF(object):
     def _text_cnn_block(x, filters, height, width, data_format='channels_last'):
         x = layers.Conv2D(filters=filters, kernel_size=(height, width),
                           strides=1, data_format=data_format)(x)
-        x = layers.BatchNormalization(axis=-1 if data_format == 'channels_last' else 1)(x)
+        x = layers.BatchNormalization(
+            axis=-1 if data_format == 'channels_last' else 1)(x)
         x = layers.Activation(activation='relu')(x)
         x = tf.reduce_max(x, axis=1, keepdims=False)
         return x
 
     @staticmethod
     def _conv1d_block(x, filters, data_format='channels_last'):
-        x = layers.Conv1D(filters=filters, kernel_size=3, strides=1, padding='same', data_format=data_format)(x)
-        x = layers.BatchNormalization(axis=-1 if data_format == 'channels_last' else 1)(x)
+        x = layers.Conv1D(filters=filters, kernel_size=3,
+                          strides=1, padding='same', data_format=data_format)(x)
+        x = layers.BatchNormalization(
+            axis=-1 if data_format == 'channels_last' else 1)(x)
         x = layers.Activation(activation='relu')(x)
         return x
 
@@ -151,10 +163,12 @@ class TF(object):
         y = tf.reshape(x, shape=(-1, self._pkt_bytes, 1))
         y = self._conv1d_block(y, filters=64)
         # y = self._conv1d_block(y, filters=64)
-        y = layers.MaxPooling1D(pool_size=2, strides=2, padding='same', data_format='channels_last')(y)
+        y = layers.MaxPooling1D(pool_size=2, strides=2,
+                                padding='same', data_format='channels_last')(y)
         y = self._conv1d_block(y, filters=128)
         # y = self._conv1d_block(y, filters=128)
-        y = layers.MaxPooling1D(pool_size=2, strides=2, padding='same', data_format='channels_last')(y)
+        y = layers.MaxPooling1D(pool_size=2, strides=2,
+                                padding='same', data_format='channels_last')(y)
         filters = 256
         y = self._conv1d_block(y, filters=filters)
         y = tf.reduce_max(y, axis=1, keepdims=False)
@@ -172,14 +186,16 @@ class TF(object):
 
         return Model(inputs=x, outputs=y)
 
-
     def _pbcnn(self):
         x = Input(shape=(self._pkt_num, self._pkt_bytes))
         y = tf.reshape(x, shape=(-1, self._pkt_num, self._pkt_bytes, 1))
         data_format = 'channels_last'
-        y1 = self._text_cnn_block(y, filters=256, height=3, width=self._pkt_bytes)
-        y2 = self._text_cnn_block(y, filters=256, height=4, width=self._pkt_bytes)
-        y3 = self._text_cnn_block(y, filters=256, height=5, width=self._pkt_bytes)
+        y1 = self._text_cnn_block(
+            y, filters=256, height=3, width=self._pkt_bytes)
+        y2 = self._text_cnn_block(
+            y, filters=256, height=4, width=self._pkt_bytes)
+        y3 = self._text_cnn_block(
+            y, filters=256, height=5, width=self._pkt_bytes)
         y = layers.concatenate(inputs=[y1, y2, y3], axis=-1)
         y = layers.Flatten(data_format=data_format)(y)
         y = layers.Dense(512, activation='relu')(y)
@@ -187,7 +203,6 @@ class TF(object):
         # y = layers.Dense(128, activation='relu')(y)
         y = layers.Dense(self._num_class, activation='linear')(y)
         return Model(inputs=x, outputs=y)
-
 
     def _init_model(self):
         if self._model_type == 'pbcnn':
@@ -217,7 +232,8 @@ class TF(object):
                        'bruteforce-web', 'dos-slowloris', 'benign', 'ddos-loic-udp', 'infiltration']
 
         cl_re = classification_report(y_true, y_pred, digits=digits,
-                                      labels=[i for i in range(self._num_class)],
+                                      labels=[i for i in range(
+                                          self._num_class)],
                                       target_names=label_names, output_dict=True)
         accuracy = round(cl_re['accuracy'], digits)
         precision = round(cl_re['macro avg']['precision'], digits)
@@ -256,7 +272,8 @@ class TF(object):
             acc_match = self._acc_func(labels, y_predict)
 
         gradients = tape.gradient(loss, self._model.trainable_variables)
-        self._optimizer.apply_gradients(zip(gradients, self._model.trainable_variables))
+        self._optimizer.apply_gradients(
+            zip(gradients, self._model.trainable_variables))
         return loss.numpy().sum(), acc_match.numpy().sum()
 
     def _test_step(self, features, labels):
@@ -289,7 +306,8 @@ class TF(object):
                     if DEBUG and steps > 300:
                         break
 
-                    loss, match = self._train_step(features, labels)  # batch loss
+                    loss, match = self._train_step(
+                        features, labels)  # batch loss
                     total_loss += loss
                     sample_count += len(features)
                     avg_train_loss = total_loss / sample_count
@@ -304,7 +322,8 @@ class TF(object):
                                      % (epoch, steps, avg_train_loss, avg_train_acc))
 
                     if valid_freq > 0 and steps % valid_freq == 0:
-                        logging.info(f'===> Step: {steps}, evaluating on VALID...')
+                        logging.info(
+                            f'===> Step: {steps}, evaluating on VALID...')
                         valid_loss, valid_acc = [], []
                         valid_cnt = 0
                         for fs, ls in self._valid_ds:
@@ -315,7 +334,8 @@ class TF(object):
 
                         avg_valid_loss = np.array(valid_loss).sum() / valid_cnt
                         avg_valid_acc = np.array(valid_acc).sum() / valid_cnt
-                        logging.info('===> VALID avg loss: %.6f, avg acc: %.6f' % (avg_valid_loss, avg_valid_acc))
+                        logging.info('===> VALID avg loss: %.6f, avg acc: %.6f' % (
+                            avg_valid_loss, avg_valid_acc))
                         self._valid_losses.append(avg_valid_loss)
                         self._valid_acc.append(avg_valid_acc)
                     steps += 1
@@ -345,10 +365,11 @@ class TF(object):
 
 def main(_):
     s = time.time()
+    print(os.getcwd())
     demo = TF(pkt_bytes=256, pkt_num=20, model='pbcnn',
-              train_path='E:\\qq_rev\\ids2018\\712_tfrecord\\712_tfrecord\\train',
-              valid_path='E:\\qq_rev\\ids2018\\712_tfrecord\\712_tfrecord\\valid',
-              test_path='E:\\qq_rev\\ids2018\\712_tfrecord\\712_tfrecord\\test',
+              train_path=r'./data/train',
+              valid_path=r'./data/valid',
+              test_path=r'./data/test',
               batch_size=1,
               num_class=15)
     # There are two models can be choose, "pbcnn" and "en_pbcnn".
@@ -361,5 +382,3 @@ def main(_):
 
 if __name__ == '__main__':
     app.run(main)
-
-
